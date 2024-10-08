d156d6c71d649d74bdceeb82be370b2328c3eedd
devbigeazi <adesholatajudeen1@gmail.com>
2024-10-08 07:50:58 +0100

    control flow📲..

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -26,9 +26,20 @@ fn main() {
     //If we want the if code block to run only when a number is not equal to 0,
     //for example, we can change the if expression to the following:
 
-    let number = 3;
-
+    let number = 8;
     if number != 2 {
         println!("The number was something other than two");
-    } 
+    }
+
+    //for multiple conditions
+    let number = 7;
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 }
