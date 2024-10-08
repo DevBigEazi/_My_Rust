1aeccd24983c9f78d3f818f80d0f26db2b091289
devbigeazi <adesholatajudeen1@gmail.com>
2024-10-08 07:17:06 +0100

    functions expression&statement😎

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -4,6 +4,14 @@ fn main() {
     anothere_function();
 
     new_function(10, 'A');
+
+    expression();
+
+    let a = five();
+    println!("The value of a is: {a}");
+
+    let x = plus_one(20);
+    println!("The value of a is: {x}");
 }
 
 fn anothere_function() {
@@ -12,4 +20,27 @@ fn anothere_function() {
 
 fn new_function(y: u8, z: char) {
     println!("The value of y and z is {y} and {z} respectively");
+}
+/*Statements and Expressions
+Function bodies are made up of a series of statements optionally ending in an
+expression.Because Rust is an expression-based language, this is an important
+distinction to understand.
+Statements are instructions that perform some action and do not return a value.
+Expressions evaluate to a resultant value. Let’s look at some examples. */
+
+fn expression() {
+    let y = {
+        let x = 5;
+        x + 2
+    };
+
+    println!("The value eof y is {y}");
+}
+
+fn five() -> i8 {
+    5
+}
+
+fn plus_one(x: i8) -> i8 {
+    x + 1
 }
